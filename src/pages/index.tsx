53489db42e7eb76260bf0276812c2c8fcca6aa0b
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2021-02-17 00:28:13 -0300

    chore: add prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
 
 export default function Home({ title = 'React Avançado' }) {
   return (
@@ -59,5 +59,5 @@ export default function Home({ title = 'React Avançado' }) {
         </a>
       </footer>
     </div>
-  )
+  );
 }
